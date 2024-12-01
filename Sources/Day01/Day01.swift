7a55317e9426b855f16054a1e871a783d64c7f46
Mathias Amnell <mathias.amnell@forefront.se>
2024-12-01 10:16:14 +0100

    Day1

--- Sources/Day01/Day01.swift
+++ Sources/Day01/Day01.swift
@@ -9,14 +9,37 @@ import AoCTools
 final class Day01: AOCDay {
     let title = "Day 1"
 
+    let leftValues: [Int]
+    let rightValues: [Int]
+
     init(input: String) {
+        var leftValues: [Int] = []
+        var rightValues: [Int] = []
+
+        input.lines.forEach { row in
+            let values = row.split(separator: "   ").map { Int($0)! }
+            leftValues.append(values[0])
+            rightValues.append(values[1])
+        }
+
+        self.leftValues = leftValues
+        self.rightValues = rightValues
     }
 
     func part1() -> Int {
-        0
+        zip(leftValues.sorted(), rightValues.sorted()).reduce(0) { acc, pair in
+            acc + abs(pair.1 - pair.0)
+        }
     }
 
     func part2() -> Int {
-        0
+        let occurencies: [Int: Int] = rightValues.reduce(into: [:]) { acc, value in
+            acc[value, default: 0] += 1
+        }
+
+        return leftValues.reduce(0) { acc, value in
+            acc + value * occurencies[value, default: 0]
+        }
+
     }
 }
