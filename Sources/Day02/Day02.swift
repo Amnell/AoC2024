cdc13c3b2b29b48ae52b447177ded53fa21c2784
Mathias Amnell <mathias.amnell@forefront.se>
2024-12-03 08:14:54 +0100

    Day 2

--- Sources/Day02/Day02.swift
+++ Sources/Day02/Day02.swift
@@ -7,15 +7,51 @@
 import AoCTools
 
 final class Day02: AOCDay {
-    
+
+    let lines: [String]
+
     init(input: String) {
+        self.lines = input.lines
     }
 
     func part1() -> Int {
-        0
+        let result = lines.compactMap {
+            let list = $0.split(separator: " ").map { Int($0)! }
+            if list.sorted(by: >) == list || list.sorted(by: <) == list {
+                let distances = zip(list, list.dropFirst()).map { abs($0 - $1) >= 1 && abs($0 - $1) <= 3 }
+                if distances.allSatisfy({ $0 }) {
+                    return 1
+                }
+                return nil
+            }
+            return nil
+        }
+        return result.count
     }
 
     func part2() -> Int {
-        0
+        let result = lines.compactMap {
+            let list = $0.split(separator: " ").map { Int($0)! }
+
+            var index = -1
+            while index < list.count {
+                var droppedList = list
+                if index >= 0 {
+                    droppedList.remove(at: index)
+                }
+
+                if droppedList.sorted(by: >) == droppedList || droppedList.sorted(by: <) == droppedList {
+                    let distances = zip(droppedList, droppedList.dropFirst()).map { abs($0 - $1) >= 1 && abs($0 - $1) <= 3 }
+                    if distances.allSatisfy({ $0 }) {
+                        return 1
+                    }
+                }
+
+                index += 1
+            }
+
+            return nil
+        }
+        return result.count
     }
 }
